ede2b50c4fdef0a68d5625b3f7f8432d97a284a8
Harshavardhana <harsha@minio.io>
2020-12-01 17:52:23 -0800

    Revert "Revert "Support variable server sets (#10314)""
    This reverts commit 790833f3b273b151ab32a7fa634f021dbb45d3b0.

--- cmd/erasure-server-sets.go
+++ cmd/erasure-server-sets.go
@@ -77,6 +77,7 @@ func newErasureServerPools(ctx context.Context, endpointServerPools EndpointServ
 			return nil, err
 		}
 		if deploymentID == "" {
+			// all zones should have same deployment ID
 			deploymentID = formats[i].ID
 		}
 		z.serverPools[i], err = newErasureSets(ctx, ep.Endpoints, storageDisks[i], formats[i])
@@ -299,7 +300,7 @@ func (z *erasureServerPools) StorageInfo(ctx context.Context, local bool) (Stora
 
 	scParity := globalStorageClass.GetParityForSC(storageclass.STANDARD)
 	if scParity == 0 {
-		scParity = z.SetDriveCount() / 2
+		scParity = getDefaultParityBlocks(z.SetDriveCount())
 	}
 
 	storageInfo.Backend.StandardSCData = z.SetDriveCount() - scParity
@@ -1429,7 +1430,6 @@ func (z *erasureServerPools) Health(ctx context.Context, opts HealthOptions) Hea
 
 	parityDrives := globalStorageClass.GetParityForSC(storageclass.STANDARD)
 	diskCount := z.SetDriveCount()
-
 	if parityDrives == 0 {
 		parityDrives = getDefaultParityBlocks(diskCount)
 	}
